c989707252047872bf10a3a4913d89d2988e47aa
Mafer <mafer_rj@ciencias.unam.mx>
2024-12-22 02:07:04 -0600

    I added messages to notify of success or failure in the server.

--- src/server/src/server.c
+++ src/server/src/server.c
@@ -28,27 +28,45 @@ void start_server(int port) {
   struct sockaddr_in server_addr;
 
   // Create the server socket
+  print_message("Creating the server socket...", 'i');
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1)
     print_message("[ERROR] Error creating socket\n", 'e');
+  else 
+    print_message("Socket succesfully created.", 'i');
 
   //Set server address
+  print_message("Configuring the server address...", 'i');
   server_addr.sin_family = AF_INET;         // Address-family IPv4
   server_addr.sin_addr.s_addr = INADDR_ANY; // Accept connections on all interfaces
   server_addr.sin_port = htons(port);       // Convert port number to network byte order
 
+  if (server_addr.sin_port == 0)
+    print_message("Error setting server port", 'e');
+  else
+    print_message("Server address configured.", 'i');
+
   // Bind the socket to the specified port
+  print_message("Binding the socket to port...", 'i');
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
     close(server_fd);
     print_message("[ERROR] Socket cannot be associated to the port\n", 'e');
-  }
+  } else
+    print_message("Socket successfully bound to the port.", 'i');
 
   // Put the server in listening mode
+  print_message("Putting the server in listening mode...", 'i');
   if(listen(server_fd, BACKLOG) == -1) {
     close(server_fd);
     print_message("[ERROR] Error listening port\n", 'e');
-  }
+  } else
+    print_message("Server is now listening for incoming connections.", 'i');
 
   printf("Server listening on port %d\n", port);
-  close(server_fd);
+
+  print_message("Closing the server socket...", 'i');
+  if (close(server_fd) == 0)
+    print_message("Server socket closed successfully.", 'i');
+  else
+    print_message("Error closing the server socket.", 'i');
 }
